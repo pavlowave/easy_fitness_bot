bcc307b52c8edf1d243948eb5fd5c483185f50f6
Pavel Sidorov <52633235+sxwiix@users.noreply.github.com>
2024-08-14 08:38:35 +0300

    com 3

--- tgbot/dispatcher.py
+++ tgbot/dispatcher.py
@@ -2,9 +2,7 @@ import re
 from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, ConversationHandler, filters
 from tgbot.handlers.onboarding.handlers import *
 from tgbot.handlers.onboarding.manage_data import *
-from tgbot.handlers.onboarding.cal_calcul import cal_cul as onboarding_handlers
 from tgbot.handlers.onboarding.cal_calcul.cal_cul import *
-
 
 def get_handlers():
     return [
@@ -12,15 +10,15 @@ def get_handlers():
         CommandHandler('hello', hello),
         CallbackQueryHandler(open_buttons, pattern=f'^{BACK_BUTTON}'),
         MessageHandler(filters.COMMAND, unknown),
-        # Добавляем обработчики для кнопок с уровнями активности
-        CallbackQueryHandler(onboarding_handlers.pick_coeff, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_12_BUTTON) + '$'),
-        CallbackQueryHandler(onboarding_handlers.pick_coeff, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_13_BUTTON) + '$'),
-        CallbackQueryHandler(onboarding_handlers.pick_coeff, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_15_BUTTON) + '$'),
-        CallbackQueryHandler(onboarding_handlers.pick_coeff, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_17_BUTTON) + '$'),
-        CallbackQueryHandler(onboarding_handlers.pick_coeff, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_19_BUTTON) + '$'),
+        # Используем объединённую функцию calorie_calculation для обработки всех этапов
+        CallbackQueryHandler(calorie_calculation, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_12_BUTTON) + '$'),
+        CallbackQueryHandler(calorie_calculation, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_13_BUTTON) + '$'),
+        CallbackQueryHandler(calorie_calculation, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_15_BUTTON) + '$'),
+        CallbackQueryHandler(calorie_calculation, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_17_BUTTON) + '$'),
+        CallbackQueryHandler(calorie_calculation, pattern='^' + re.escape(ACTIVITY_COEFFICIENT_19_BUTTON) + '$'),
         CallbackQueryHandler(calorie_calculation, pattern='^' + re.escape(MAN_BUTTON) + '$'),
         CallbackQueryHandler(calorie_calculation, pattern='^' + re.escape(WOMAN_BUTTON) + '$'),
-        MessageHandler(filters.TEXT & ~filters.COMMAND, onboarding_handlers.handle_user_input),
+        MessageHandler(filters.TEXT & ~filters.COMMAND, calorie_calculation),
 
         CallbackQueryHandler(button_base_click),
         ConversationHandler(
@@ -28,19 +26,18 @@ def get_handlers():
             states={
                 START: [],
                 AWAITING_WEIGHT: [
-                    MessageHandler(filters.TEXT, onboarding_handlers.handle_user_input),
+                    MessageHandler(filters.TEXT, calorie_calculation),
                 ],
                 AWAITING_HEIGHT: [
-                    MessageHandler(filters.TEXT, onboarding_handlers.handle_user_input),
+                    MessageHandler(filters.TEXT, calorie_calculation),
                 ],
                 AWAITING_AGE: [
-                    MessageHandler(filters.TEXT, onboarding_handlers.handle_user_input),
+                    MessageHandler(filters.TEXT, calorie_calculation),
                 ],
                 AWAITING_ACTIVITY_COEFFICIENT: [
-                    MessageHandler(filters.TEXT, onboarding_handlers.handle_user_input),
+                    MessageHandler(filters.TEXT, calorie_calculation),
                 ],
             },
             fallbacks=[],
         )
     ]
-
--- tgbot/handlers/onboarding/cal_calcul/cal_cul.py
+++ tgbot/handlers/onboarding/cal_calcul/cal_cul.py
@@ -1,16 +1,18 @@
 import asyncio
 import logging
-import re
-from asgiref.sync import sync_to_async
-from django.db.models import QuerySet
 from telegram import Update
 from telegram.constants import ParseMode
-from telegram.ext import ContextTypes, CallbackContext
-
+from telegram.ext import ContextTypes
 from tgbot.handlers.onboarding.keyboards import *
 from tgbot.handlers.utils.info import extract_user_data_from_update
-from users.models import User
 from tgbot.handlers.onboarding.manage_data import *
+
+from tgbot.handlers.onboarding.keyboards import make_keyboard_for_start_command
+
+from tgbot.handlers.onboarding.keyboards import activity_coefficient
+from tgbot.handlers.onboarding.manage_data import WOMAN_BUTTON, MAN_BUTTON, ACTIVITY_COEFFICIENT_12_BUTTON, \
+    ACTIVITY_COEFFICIENT_13_BUTTON, ACTIVITY_COEFFICIENT_15_BUTTON, ACTIVITY_COEFFICIENT_17_BUTTON, \
+    ACTIVITY_COEFFICIENT_19_BUTTON
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -20,199 +22,140 @@ logger = logging.getLogger(__name__)
 
 # Определение состояний
 (AWAITING_WEIGHT, AWAITING_HEIGHT,
-    AWAITING_AGE, AWAITING_ACTIVITY_COEFFICIENT) = range(4)
+ AWAITING_AGE, AWAITING_ACTIVITY_COEFFICIENT) = range(4)
 
 async def calorie_calculation(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
-    data = query.data
     user_id = extract_user_data_from_update(update)['user_id']
-    message = update.callback_query.message
-
-    if data in [MAN_BUTTON, WOMAN_BUTTON]:
-        if data == MAN_BUTTON:
-            context.user_data['gender'] = 'male'
-        else:
-            context.user_data['gender'] = 'female'
-
-        # Задержка перед удалением старого сообщения
-        await asyncio.sleep(0.4)
-        # Удаление старого сообщения
-        await context.bot.delete_message(
-            chat_id=user_id,
-            message_id=message.message_id
-        )
-
-        # Задержка перед отправкой нового сообщения
-        await asyncio.sleep(0.4)
-
-        # Отправка нового сообщения
-        text = 'Введите вес в кг, на который Вы хотите посчитать калорийность:'
-        new_message = await context.bot.send_message(
-            chat_id=user_id,
-            text=text,
-            parse_mode=ParseMode.HTML,
-        )
-
-        # Обновление состояния пользователя
-        context.user_data['current_state'] = AWAITING_WEIGHT
-
-
-async def handle_user_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global true_weight, true_height, true_age, basal_metabolism
-    user_id = extract_user_data_from_update(update)['user_id']
-    user_text = update.message.text.title()
-    gender = context.user_data.get('gender')
     current_state = context.user_data.get('current_state')
 
-    # Сохранение идентификатора старого сообщения
-    old_message_id = context.user_data.get('current_message_id')
+    if query:
+        data = query.data
+        if data in [MAN_BUTTON, WOMAN_BUTTON]:
+            context.user_data['gender'] = 'male' if data == MAN_BUTTON else 'female'
 
-    if old_message_id:
-        # Удаление старого сообщения
-        await asyncio.sleep(0.4)
-        try:
-            await context.bot.delete_message(
-                chat_id=user_id,
-                message_id=old_message_id
-            )
-        except Exception as e:
-            logging.error(f"Failed to delete message: {e}")
+            await asyncio.sleep(0.4)
+            await query.message.delete()
 
-    if current_state == AWAITING_WEIGHT:
-        if not user_text.isdigit():
+            await asyncio.sleep(0.4)
             new_message = await context.bot.send_message(
-                text='Введенный текст не содержит цифры. Пожалуйста, введите Ваш вес в цифрах:',
                 chat_id=user_id,
+                text='Введите вес в кг, на который Вы хотите посчитать калорийность:',
                 parse_mode=ParseMode.HTML,
             )
-            context.user_data['current_message_id'] = new_message.message_id
             context.user_data['current_state'] = AWAITING_WEIGHT
+            context.user_data['current_message_id'] = new_message.message_id
             return
 
-        weight = int(user_text)
+    if update.message:
+        user_text = update.message.text.title()
+        old_message_id = context.user_data.get('current_message_id')
 
-        if weight > 250:
+        if old_message_id:
+            await asyncio.sleep(0.4)
+            try:
+                await context.bot.delete_message(chat_id=user_id, message_id=old_message_id)
+            except Exception as e:
+                logging.error(f"Failed to delete message: {e}")
+
+        if current_state == AWAITING_WEIGHT:
+            if not user_text.isdigit():
+                new_message = await context.bot.send_message(
+                    chat_id=user_id,
+                    text='Введенный текст не содержит цифры. Пожалуйста, введите Ваш вес в цифрах:',
+                    parse_mode=ParseMode.HTML,
+                )
+                context.user_data['current_message_id'] = new_message.message_id
+                return
+
+            weight = int(user_text)
+            if weight > 250 or weight <= 5:
+                new_message = await context.bot.send_message(
+                    chat_id=user_id,
+                    text=f'Введен некорректный вес. Пожалуйста, введите значение {"до 250 кг" if weight > 250 else "более 5 кг"}:',
+                    parse_mode=ParseMode.HTML,
+                )
+                context.user_data['current_message_id'] = new_message.message_id
+                return
+
+            context.user_data['weight'] = weight
             new_message = await context.bot.send_message(
-                text='Введен некорректный вес. Пожалуйста, введите значение до 250 кг:',
                 chat_id=user_id,
+                text='Введите ваш рост в сантиметрах:',
                 parse_mode=ParseMode.HTML,
             )
-            context.user_data['current_message_id'] = new_message.message_id
-            context.user_data['current_state'] = AWAITING_WEIGHT
-            return
-        elif weight <= 5:
-            new_message = await context.bot.send_message(
-                text='Введен некорректный вес. Пожалуйста, введите значение более 5 кг:',
-                chat_id=user_id,
-                parse_mode=ParseMode.HTML,
-            )
-            context.user_data['current_message_id'] = new_message.message_id
-            context.user_data['current_state'] = AWAITING_WEIGHT
-            return
-
-        true_weight = weight
-        new_message = await context.bot.send_message(
-            text='Введите ваш рост в сантиметрах:',
-            chat_id=user_id,
-            parse_mode=ParseMode.HTML,
-        )
-        context.user_data['current_message_id'] = new_message.message_id
-        context.user_data['current_state'] = AWAITING_HEIGHT
-
-    elif current_state == AWAITING_HEIGHT:
-        if not user_text.isdigit():
-            new_message = await context.bot.send_message(
-                text='Введенный текст не содержит цифры. Пожалуйста, введите Ваш рост в цифрах:',
-                chat_id=user_id,
-                parse_mode=ParseMode.HTML,
-            )
-            context.user_data['current_message_id'] = new_message.message_id
             context.user_data['current_state'] = AWAITING_HEIGHT
-            return
+            context.user_data['current_message_id'] = new_message.message_id
 
-        height = int(user_text)
-        if height > 300:
+        elif current_state == AWAITING_HEIGHT:
+            if not user_text.isdigit():
+                new_message = await context.bot.send_message(
+                    chat_id=user_id,
+                    text='Введенный текст не содержит цифры. Пожалуйста, введите Ваш рост в цифрах:',
+                    parse_mode=ParseMode.HTML,
+                )
+                context.user_data['current_message_id'] = new_message.message_id
+                return
+
+            height = int(user_text)
+            if height > 300 or height <= 30:
+                new_message = await context.bot.send_message(
+                    chat_id=user_id,
+                    text=f'Введен некорректный рост. Пожалуйста, введите значение {"до 300 см" if height > 300 else "более 30 см"}:',
+                    parse_mode=ParseMode.HTML,
+                )
+                context.user_data['current_message_id'] = new_message.message_id
+                return
+
+            context.user_data['height'] = height
             new_message = await context.bot.send_message(
-                text='Введен некорректный рост. Пожалуйста, введите корректные данные:',
                 chat_id=user_id,
+                text='Введите ваш возраст:',
                 parse_mode=ParseMode.HTML,
             )
-            context.user_data['current_message_id'] = new_message.message_id
-            context.user_data['current_state'] = AWAITING_HEIGHT
-            return
-        elif height <= 30:
-            new_message = await context.bot.send_message(
-                text='Введен некорректный рост. Пожалуйста, введите значение более 30 см:',
-                chat_id=user_id,
-                parse_mode=ParseMode.HTML,
-            )
-            context.user_data['current_message_id'] = new_message.message_id
-            context.user_data['current_state'] = AWAITING_HEIGHT
-            return
-
-        true_height = height
-        new_message = await context.bot.send_message(
-            text='Введите ваш возраст:',
-            chat_id=user_id,
-            parse_mode=ParseMode.HTML,
-        )
-        context.user_data['current_message_id'] = new_message.message_id
-        context.user_data['current_state'] = AWAITING_AGE
-
-    elif current_state == AWAITING_AGE:
-        if not user_text.isdigit():
-            new_message = await context.bot.send_message(
-                text='Введенный текст не содержит цифры. Пожалуйста, введите Ваш возраст в цифрах:',
-                chat_id=user_id,
-                parse_mode=ParseMode.HTML,
-            )
-            context.user_data['current_message_id'] = new_message.message_id
             context.user_data['current_state'] = AWAITING_AGE
-            return
-
-        age = int(user_text)
-        if age > 120:
-            new_message = await context.bot.send_message(
-                text='Введен некорректный возраст. Пожалуйста, введите корректные данные:',
-                chat_id=user_id,
-                parse_mode=ParseMode.HTML,
-            )
             context.user_data['current_message_id'] = new_message.message_id
-            context.user_data['current_state'] = AWAITING_AGE
-            return
-        elif age <= 1:
+
+        elif current_state == AWAITING_AGE:
+            if not user_text.isdigit():
+                new_message = await context.bot.send_message(
+                    chat_id=user_id,
+                    text='Введенный текст не содержит цифры. Пожалуйста, введите Ваш возраст в цифрах:',
+                    parse_mode=ParseMode.HTML,
+                )
+                context.user_data['current_message_id'] = new_message.message_id
+                return
+
+            age = int(user_text)
+            if age > 120 or age <= 1:
+                new_message = await context.bot.send_message(
+                    chat_id=user_id,
+                    text=f'Введен некорректный возраст. Пожалуйста, введите значение {"до 120 лет" if age > 120 else "более 1 года"}:',
+                    parse_mode=ParseMode.HTML,
+                )
+                context.user_data['current_message_id'] = new_message.message_id
+                return
+
+            context.user_data['age'] = age
+            gender = context.user_data.get('gender')
+            weight = context.user_data.get('weight')
+            height = context.user_data.get('height')
+
+            basal_metabolism = int(10 * weight) + (6.25 * height) - (5 * age) + (5 if gender == 'male' else -161)
+            context.user_data['basal_metabolism'] = basal_metabolism
+
             new_message = await context.bot.send_message(
-                text='Введен некорректный возраст. Пожалуйста, введите значение более 1 года:',
                 chat_id=user_id,
+                text=(f'Ваш основной обмен ≈ {basal_metabolism} ккал\n'
+                      f'Теперь выберите уровень вашей физической активности:'),
                 parse_mode=ParseMode.HTML,
+                reply_markup=activity_coefficient()
             )
+            context.user_data['current_state'] = AWAITING_ACTIVITY_COEFFICIENT
             context.user_data['current_message_id'] = new_message.message_id
-            context.user_data['current_state'] = AWAITING_AGE
-            return
 
-        true_age = age
-        if gender == 'male':
-            basal_metabolism = int(10 * true_weight) + (6.25 * true_height) - (5 * true_age) + 5
-        elif gender == 'female':
-            basal_metabolism = int(10 * true_weight) + (6.25 * true_height) - (5 * true_age) - 161
-
-        new_message = await context.bot.send_message(
-            text=(f'Ваш основной обмен ≈ {basal_metabolism} ккал\n'
-                  f'Теперь выберите уровень вашей физической активности:'),
-            chat_id=user_id,
-            parse_mode=ParseMode.HTML,
-            reply_markup=activity_coefficient()
-        )
-        context.user_data['current_message_id'] = new_message.message_id
-        context.user_data['current_state'] = AWAITING_ACTIVITY_COEFFICIENT
-
-
-async def pick_coeff(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
-    query.answer()
-    data = query.data
-    print(basal_metabolism)
-    if context.user_data.get('current_state') == AWAITING_ACTIVITY_COEFFICIENT:
+    elif current_state == AWAITING_ACTIVITY_COEFFICIENT:
+        data = query.data
         coefficients = {
             ACTIVITY_COEFFICIENT_12_BUTTON: 1.2,
             ACTIVITY_COEFFICIENT_13_BUTTON: 1.375,
@@ -220,13 +163,26 @@ async def pick_coeff(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
             ACTIVITY_COEFFICIENT_17_BUTTON: 1.725,
             ACTIVITY_COEFFICIENT_19_BUTTON: 1.9
         }
-        coefficient = coefficients.get(data)  # Получаем соответствующий коэффициент из словаря
+
+        coefficient = coefficients.get(data)
+        basal_metabolism = context.user_data.get('basal_metabolism')
+
+        # Удаляем предыдущее сообщение, содержащее коэффициенты
+        old_message_id = context.user_data.get('current_message_id')
+
+        if old_message_id:
+            await asyncio.sleep(0.4)
+            try:
+                await context.bot.delete_message(chat_id=user_id, message_id=old_message_id)
+            except Exception as e:
+                logging.error(f"Failed to delete message: {e}")
+
         if coefficient is not None:
-            calorie_intake = coefficient * basal_metabolism
-            calorie_intake = int(calorie_intake)
-            user_id = extract_user_data_from_update(update)['user_id']
-            await context.bot.send_message(
+            calorie_intake = int(coefficient * basal_metabolism)
+            new_message = await context.bot.send_message(
+                chat_id=user_id,
                 text=(
+
                     f'Рекомендуемая калорийность рациона: {calorie_intake} ккал.\n'
                     f'Полученные цифры позволяют рассчитать необходимую калорийность рациона:\n'
                     f'- для поддержания массы тела поступление калорий должно равняться рассчитанным затратам;\n'
@@ -240,10 +196,10 @@ async def pick_coeff(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
                     f'10-14 дней нужно потреблять рассчитанное количество калорий и после этого оценить изменение массы тела.\n'
                     f'Если масса стабильна, рацион сбалансирован по энергии.\n'
                     f'Если она уменьшается — имеется дефицит калорий, если растет — профицит.'
+
                 ),
-                chat_id=user_id,
                 parse_mode=ParseMode.HTML,
-                reply_markup=make_keyboard_for_start_command(),
+                reply_markup=make_keyboard_for_start_command()
             )
-        else:
-            pass
+            context.user_data['current_message_id'] = new_message.message_id
+
